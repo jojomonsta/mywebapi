669511c2adafcd543778ad202d6480af01b3ea1f
Johanna Turner <johanna.turner@optimumcredit.co.uk>
2019-03-11 15:58:48 +0000

    Add Swagger

--- mywebapi/App_Start/WebApiConfig.cs
+++ mywebapi/App_Start/WebApiConfig.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.Application;
 
 namespace mywebapi
 {
@@ -9,10 +13,27 @@ namespace mywebapi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.Formatters.Clear();
+            var json = new JsonMediaTypeFormatter
+            {
+                SerializerSettings =
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    Formatting = Formatting.Indented
+                }
+            };
+            config.Formatters.Add(json);
+
+            //redirects to swagger if no endpoint is hit (prevents annoying forbidden error)
+            config.Routes.MapHttpRoute(
+                name: "swagger_root",
+                routeTemplate: "",
+                defaults: null,
+                constraints: null,
+                handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
